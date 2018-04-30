4a45bc2c39e8a7fbfba10f60be61ad24817b4fe6
VolkerSchiewe <volker.s1994@gmail.com>
2018-04-30 16:47:33 +0200

    delta encoding in frame groups

--- models/frame_group.py
+++ models/frame_group.py
@@ -1,9 +1,16 @@
+from models.frame import Frame
+
+
 class FrameGroup:
-    def __init__(self, trajectory_id, frame_group_id, i_frame):
+    def __init__(self, trajectory_id: int, frame_group_id: int, i_frame: Frame, p_frames: [Frame]):
         self.trajectory_id = trajectory_id
         self.frame_group_id = frame_group_id
         self.i_frame = i_frame
-        self.p_frames = []
+        self.p_frames = p_frames
 
     def delta_encoding(self):
-        pass
+        raw_x = self.i_frame.x
+        raw_y = self.i_frame.y
+        for frame in self.p_frames:
+            frame.x = frame.x - raw_x
+            frame.y = frame.y - raw_y
