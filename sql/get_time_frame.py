539fdac2c9c0b0c9475099a3e7496fb46f427146
Galbaxx <victor.kuenstler@outlook.com>
2018-05-02 12:18:03 +0200

    Refactoring frame trajectory request

--- sql/get_time_frame.py
+++ sql/get_time_frame.py
@@ -1,11 +1,12 @@
 from consts import DB_TABLE
 
+
 def get_time_frame(frame_group_id, frame_id, top_k=None):
     # TODO change when column names were renamed
-    if top_k == None:
-        top_string = ''
-    else:
+    top_string = ''
+    if top_k:
         top_string = f'''TOP {top_k}'''
+
     sql = f'''
     SELECT {top_string} FGID,
     Ix + P{frame_id}x as LON,
