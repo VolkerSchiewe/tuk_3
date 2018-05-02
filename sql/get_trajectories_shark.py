f45c7fb33c2f44c9769c87cc47c57716859bd660
Galbaxx <victor.kuenstler@outlook.com>
2018-05-02 14:29:51 +0200

    Add getting trajectory for timeframes (fgid range and fid range)

---
+++ sql/get_trajectories_shark.py
@@ -0,0 +1,40 @@
+from consts import DB_TABLE
+
+
+def get_trajs_in_frame(fgid, fid, top_k=None):
+    # TODO change column names when column names were renamed
+    top_string = ''
+    if top_k:
+        top_string = f'''TOP {top_k}'''
+
+    sql = f'''
+    SELECT {top_string} FGID,
+    Ix + P{fid}x as LON,
+    Iy + P{fid}y as LAT
+    FROM {DB_TABLE}
+    WHERE TID = {fgid}
+    '''
+    return sql
+
+def get_trajs_in_frame_range(first_fgid, last_fgid, first_fid, last_fid, top_k=None):
+    # TODO change column names when column names were renamed
+    top_string = ''
+    if top_k:
+        top_string = f'''TOP {top_k}'''
+
+    sql = ''
+    filter_string = f'''WHERE TID >= {first_fgid} AND TID <= {last_fgid}'''
+    union_string = 'UNION ALL'
+
+    for i in range(first_fid, last_fid + 1):
+        sql += f'''
+        SELECT {top_string} FGID,
+        Ix + P{i}x as LON,
+        Iy + P{i}y as LAT
+        FROM {DB_TABLE}
+        {filter_string} 
+        '''
+        if i != last_fid:
+            sql += union_string
+
+    return sql
