7c457d2e2cf6cb835ed71873e82ac0c6e5afcbb6
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-05-05 17:22:55 -0700

    Add type information to model classes

--- models/sample.py
+++ models/sample.py
@@ -1,23 +1,25 @@
+import datetime
+
 from models.frame import Frame
 
 
 class Sample:
-    def __init__(self, timestamp, x, y, speed, occupancy):
+    def __init__(self, timestamp: datetime.time, x: float, y: float, speed: int, occupancy: int):
         self.timestamp = timestamp
         self.x = x
         self.y = y
         self.speed = speed
         self.occupancy = occupancy
 
+    @classmethod
+    def from_row(cls, row):
+        return Sample(row[1], row[2], row[3], row[4], row[5])
+
     def to_frame(self):
         return Frame(self.frame_id(), self.x, self.y)
 
     def frame_id(self):
         return self.timestamp.hour * 60 + self.timestamp.minute
-
-    @classmethod
-    def from_tuple(cls, tuple):
-        return Sample(tuple[1], tuple[2], tuple[3], tuple[4], tuple[5])
 
     def __eq__(self, other):
         """Overrides the default implementation"""
