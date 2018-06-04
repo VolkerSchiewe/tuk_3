937e226efb235b5d102203af5f50db8633ac7ee5
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-06-04 16:59:16 +0200

    Add trip frame format

--- models/frame_group.py
+++ models/frame_group.py
@@ -2,9 +2,12 @@ from models.frame import Frame
 
 
 class FrameGroup:
-    def __init__(self, trajectory_id: int, frame_group_id: int, i_frame: Frame, p_frames: [Frame]):
+    def __init__(self, trajectory_id: int, frame_group_id: int, trip_id: int, occupancy: int, i_frame: Frame,
+                 p_frames: [Frame]):
         self.trajectory_id = trajectory_id
         self.frame_group_id = frame_group_id
+        self.trip_id = trip_id
+        self.occupancy = bool(occupancy)
         self.i_frame = i_frame
         self.p_frames = p_frames
 
